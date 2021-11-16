0b2e66647cc773f0bba3301aedb8f6ca70c2b545
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-16 16:10:22 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+const one = <h1>Hi, my name is</h1>;
+const two = <h2 className="big-heading">Mohamed Aziz Laouiti.</h2>;
+const three = <h3 className="big-heading"></h3>;
+const four = (
+  <>
+    <p>
+      I’m a Full-Stack software engineer specializing in
+      designing and building secure REST APIs.
+      Well versed in several programming languages such
+      as javascript , ruby and python, 
+      with a constant drive for self-development, and learning
+      new technologies and languages
+      <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
+        Upstatement
+      </a>
+      .
+    </p>
+  </>
+);
+const five = (
+  <a
+    className="email-link"
+    href="https://www.newline.co/courses/build-a-spotify-connected-app"
+    target="_blank"
+    rel="noreferrer">
+    Check out my course!
+  </a>
+);
+const items = [one, two, three, four, five];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
   );
 }
 
